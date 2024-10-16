9ddc329583c5c1517df8364cf9ae5e988b3c3364
dmacd1 <dicd080530mgtghla5@cbtis217.edu.mx>
2024-10-16 16:27:27 -0600

    Codigo inicial

---
+++ cuadratica.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double a,b,c, x1, x2,x;
+    cin>>a>>b>>c;
+    x1= (-1*b)+sqrt((b*b)-4*(a*c))/(2*a);
+    x2= (-1*b)-sqrt((b*b)-4*(a*c))/(2*a);
+    cout<<"X1= "<<x1<<endl;
+    cout<<"X2= "<<x2<<endl;
+    return 0;
+}
+
